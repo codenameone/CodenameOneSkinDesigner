db910a144e5a5cb089e9d25c58cb274c201a7458
Shai Almog <shai@codenameone.com>
2016-05-29 07:23:23 +0300

    Added better validation

--- src/com/codename1/tools/skindesigner/SkinDesigner.java
+++ src/com/codename1/tools/skindesigner/SkinDesigner.java
@@ -36,6 +36,7 @@ import com.codename1.ui.Label;
 import com.codename1.ui.plaf.UIManager;
 import com.codename1.ui.util.Resources;
 import com.codename1.io.Log;
+import com.codename1.io.Preferences;
 import com.codename1.io.Properties;
 import com.codename1.io.Storage;
 import com.codename1.io.Util;
@@ -48,6 +49,7 @@ import com.codename1.ui.FontImage;
 import com.codename1.ui.Graphics;
 import com.codename1.ui.Image;
 import com.codename1.ui.Tabs;
+import com.codename1.ui.TextArea;
 import com.codename1.ui.TextField;
 import com.codename1.ui.Toolbar;
 import com.codename1.ui.events.ActionEvent;
@@ -58,6 +60,9 @@ import com.codename1.ui.layouts.LayeredLayout;
 import com.codename1.ui.plaf.Style;
 import com.codename1.ui.spinner.Picker;
 import com.codename1.ui.util.ImageIO;
+import com.codename1.ui.validation.NumericConstraint;
+import com.codename1.ui.validation.RegexConstraint;
+import com.codename1.ui.validation.Validator;
 import com.codename1.util.Base64;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -85,7 +90,37 @@ public class SkinDesigner {
         Image getSkinImage();
     }
     
-    private ImageSettings createImageSettings(String imageFile) {
+    private void autoSave(TextArea ta, String preferencesKey) {
+        String val = Preferences.get(preferencesKey, null);
+        if(val != null) {
+            ta.setText(val);
+        }
+        ta.addActionListener(e -> {
+            Preferences.set(preferencesKey, ta.getText());
+        });
+    }
+
+    private void autoSave(OnOffSwitch o, String preferencesKey) {
+        boolean val = Preferences.get(preferencesKey, o.isValue());
+        if(val != o.isValue()) {
+            o.setValue(val);
+        }
+        o.addActionListener(e -> {
+            Preferences.set(preferencesKey, o.isValue());
+        });
+    }
+
+    private void autoSave(Picker p, String preferencesKey) {
+        String val = Preferences.get(preferencesKey, null);
+        if(val != null) {
+            p.setSelectedString(val);
+        }
+        p.addActionListener(e -> {
+            Preferences.set(preferencesKey, p.getSelectedString());
+        });
+    }
+    
+    private ImageSettings createImageSettings(String imageFile, String prefix, Validator vl) {
         Image img = null;
         try {
             img = Image.createImage(Display.getInstance().getResourceAsStream(getClass(), imageFile));
@@ -98,19 +133,37 @@ public class SkinDesigner {
             Display.getInstance().openGallery((ee) -> {
                     if(ee != null && ee.getSource() != null) {
                         try {
-                            sl.setIcon(Image.createImage((String)ee.getSource()));
+                            String fileName = (String)ee.getSource();
+                            sl.setIcon(Image.createImage(fileName));
                             sl.getParent().revalidate();
+                            Util.copy(FileSystemStorage.getInstance().openInputStream(fileName), 
+                                    Storage.getInstance().createOutputStream(prefix + ".png"));
                         } catch(IOException err) {
                             ToastBar.showErrorMessage("Error Loading Image: " + err);
                         }
                     }
                 }, Display.GALLERY_IMAGE);
         });
+        if(Storage.getInstance().exists(prefix + ".png")) {
+            try(InputStream is = Storage.getInstance().createInputStream(prefix + ".png")) {
+                sl.setIcon(Image.createImage(is));
+            } catch(IOException err) {
+                Log.e(err);
+            }
+        }
         
         final TextField screenWidthPixels = new TextField("320", "Width", 8, TextField.NUMERIC);
         final TextField screenHeightPixels = new TextField("480", "Height", 8, TextField.NUMERIC);
         final TextField screenPositionX = new TextField("40", "X", 8, TextField.NUMERIC);
         final TextField screenPositionY = new TextField("40", "Y", 8, TextField.NUMERIC);
+        autoSave(screenWidthPixels, prefix + "Width");
+        autoSave(screenHeightPixels, prefix + "Height");
+        autoSave(screenPositionX, prefix + "X");
+        autoSave(screenPositionY, prefix + "Y");
+        vl.addConstraint(screenWidthPixels, new NumericConstraint(false, 20, 5000, "Screen size must be a valid integer in the 20-5000 range")).
+                addConstraint(screenHeightPixels, new NumericConstraint(false, 20, 5000, "Screen size must be a valid integer in the 20-5000 range")).
+                addConstraint(screenPositionX, new NumericConstraint(false, 20, 5000, "Screen size must be a valid integer in the 20-5000 range")).
+                addConstraint(screenPositionY, new NumericConstraint(false, 20, 5000, "Screen size must be a valid integer in the 20-5000 range"));
         
         Button aim = new Button();
         FontImage.setMaterialIcon(aim, FontImage.MATERIAL_PAN_TOOL);
@@ -297,10 +350,11 @@ public class SkinDesigner {
             return;
         }
         Form skinDesignerForm = new Form("Skin Designer", new BorderLayout());
+        Validator vl = new Validator();
         final Tabs details = new Tabs();
         Style titleCommand = UIManager.getInstance().getComponentStyle("Command");
-        ImageSettings imPortrait = createImageSettings("/skin.png");
-        ImageSettings imLandscape = createImageSettings("/skin_l.png");
+        ImageSettings imPortrait = createImageSettings("/skin.png", "port", vl);
+        ImageSettings imLandscape = createImageSettings("/skin_l.png", "lan", vl);
         
         skinDesignerForm.add(BorderLayout.CENTER, details);
         
@@ -308,39 +362,52 @@ public class SkinDesigner {
         nativeTheme.setStrings(NATIVE_THEMES);
         nativeTheme.setSelectedString(NATIVE_THEMES[0]);
         nativeTheme.setRenderingPrototype("XXXXXXXXXXXXXXXXXXX");
+        autoSave(nativeTheme, "nativeTheme");
 
         Picker platformName = new Picker();
         platformName.setStrings("ios", "and", "win","rim", "se");
         platformName.setSelectedString("ios");
         platformName.setRenderingPrototype("XXXX");
+        autoSave(platformName, "platformName");
 
         OnOffSwitch tablet = new OnOffSwitch();
         tablet.setValue(false);
+        autoSave(tablet, "tablet");
 
         TextField systemFontFamily = new TextField("Helvetica", "System Font Family", 20, TextField.ANY);
         TextField proportionalFontFamily = new TextField("Helvetica", "Proportional Font Family", 20, TextField.ANY);
         TextField monospaceFontFamily = new TextField("Courier", "Monospace Font Family", 20, TextField.ANY);
+        autoSave(systemFontFamily, "systemFontFamily");
+        autoSave(proportionalFontFamily, "proportionalFontFamily");
+        autoSave(monospaceFontFamily, "monospaceFontFamily");
         
         TextField smallFontSize = new TextField("11", "Small Font Size", 20, TextField.NUMERIC);
         TextField mediumFontSize = new TextField("14", "Medium Font Size", 20, TextField.NUMERIC);
         TextField largeFontSize = new TextField("20", "Large Font Size", 20, TextField.NUMERIC);
+        autoSave(smallFontSize, "smallFontSize");
+        autoSave(mediumFontSize, "mediumFontSize");
+        autoSave(largeFontSize, "largeFontSize");
         
         TextField pixelRatio = new TextField("6.4173236936575", "Pixel Ratio - pixels per millimeter", 20, TextField.DECIMAL);
+        autoSave(pixelRatio, "pixelRatio");
         
         Picker overrideNamePrimary = new Picker();
         overrideNamePrimary.setStrings("phone", "tablet", "desktop");
         overrideNamePrimary.setSelectedString("phone");
         overrideNamePrimary.setRenderingPrototype("XXXXXXXX");
+        autoSave(overrideNamePrimary, "overrideNamePrimary");
 
         Picker overrideNameSecondary = new Picker();
         overrideNameSecondary.setStrings("ios", "android", "windows");
         overrideNameSecondary.setSelectedString("ios");
         overrideNameSecondary.setRenderingPrototype("XXXXXXXX");
+        autoSave(overrideNameSecondary, "overrideNameSecondary");
         
         Picker overrideNameLast = new Picker();
         overrideNameLast.setStrings("iphone", "ipad", "android-phone", "android-tablet", "desktop");
         overrideNameLast.setSelectedString("iphone");
         overrideNameLast.setRenderingPrototype("XXXXXXXX");
+        autoSave(overrideNameLast, "overrideNameLast");
         
         
         Container settingsContainer = BoxLayout.encloseY(
@@ -381,10 +448,15 @@ public class SkinDesigner {
         details.setTabSelectedIcon(0, portraitIconSel);
         details.setTabSelectedIcon(1, landscapeIconSel);
         details.setTabSelectedIcon(2, settingsIconSel);
-        
+        vl.addConstraint(smallFontSize, new NumericConstraint(false, 5, 400, "Font size must be a valid integer in the 5-400 range")).
+                addConstraint(mediumFontSize, new NumericConstraint(false, 5, 400, "Font size must be a valid integer in the 5-400 range")).
+                addConstraint(largeFontSize, new NumericConstraint(false, 5, 400, "Font size must be a valid integer in the 5-400 range")).
+                addConstraint(pixelRatio, new NumericConstraint(true, 0.1, 60, "PixelRatio is a positive decimal size in the range of 0.1 to 60")).
+                setShowErrorMessageForFocusedComponent(true);
+                
         ShouldExecute s = NativeLookup.create(ShouldExecute.class);
         if(s != null && s.isSupported()) {
-            skinDesignerForm.getToolbar().addCommandToRightBar("", 
+            Command saveCommand = skinDesignerForm.getToolbar().addCommandToRightBar("", 
                     FontImage.createMaterial(FontImage.MATERIAL_SAVE, titleCommand), e -> {
                         byte[] data = createSkinFile(imPortrait, imLandscape, nativeTheme, platformName, tablet, systemFontFamily, 
                                 proportionalFontFamily, monospaceFontFamily, smallFontSize, mediumFontSize, largeFontSize, 
@@ -403,6 +475,7 @@ public class SkinDesigner {
                             }
                         }
                     });
+            vl.addSubmitButtons(skinDesignerForm.getToolbar().findCommandComponent(saveCommand));
         }
 
         if(Display.getInstance().isNativeShareSupported()) {
